f33b8d6af7389dee41e898bbb37aeb80274e0f9c
abhilashgahankari <abhigahankari@gmail.com>
2020-06-07 15:16:31 +0530

    Added developers page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
+import Profiles from "./components/profiles/Profiles";
 
 import "./App.css";
 
@@ -40,6 +41,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
