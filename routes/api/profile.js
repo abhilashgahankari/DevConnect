6eccda26b83bdb3bcfaf90d6a10bc6741ff3853b
abhilashgahankari <abhigahankari@gmail.com>
2020-07-17 22:25:29 +0530

    Changed github access and axios

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../..//models/Profile");
@@ -336,14 +337,13 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 router.get("/github/:username", (req, res) => {
   try {
     const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        "githubClientId"
-      )}&client_secret=${config.get("githubSecret")}`,
+      uri: encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      ),
       method: "GET",
       headers: {
         "user-agent": "node.js",
+        Authorization: `token ${config.get("githubToken")}`,
       },
     };
 
